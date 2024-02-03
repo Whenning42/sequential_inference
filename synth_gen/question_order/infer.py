b6763c181d27951c51337466ee620a67ed728207
William Henning <wchenningwchenning@gmail.com>
2024-02-02 23:37:38 -0500

    Switch to batched inference

--- synth_gen/question_order/infer.py
+++ synth_gen/question_order/infer.py
@@ -13,12 +13,14 @@ client = OpenAI(
 )
 
 
-def infer(prompt: str) -> str:
+def infer(prompts: list[str]) -> str:
     response = client.completions.create(
         model="gpt-3.5-turbo-instruct",
-        prompt=prompt,
+        prompt=prompts,
+        max_tokens=30,
+        temperature=0.0,
     )
-    return response.choices[0].text.strip()
+    return [c.text.strip() for c in response.choices]
 
 
 def get_value(response: str) -> Optional[int]:
@@ -30,20 +32,27 @@ def get_value(response: str) -> Optional[int]:
         return None
 
 
-ds = left_right_prompting.generate_ds()
+ds = left_right_prompting.generate_ds(n=800)
+print(len(ds))
 
 datapoints = []
-for i in tqdm.tqdm(range(len(ds))):
-    response = infer(ds[i].prompt)
-    value = get_value(response)
-    datapoint = {
-        "prompt": ds[i].prompt,
-        "response": response,
-        "value": value,
-        "gt_answer": ds[i].answer,
-        "question_def": ds[i].__dict__,
-    }
-    datapoints.append(datapoint)
+BS = 20
+for i in tqdm.tqdm(range(len(ds) // BS)):
+    batch = ds[BS * i : BS * (i + 1)]
+    prompts = [b.prompt for b in batch]
+    responses = infer(prompts)
+    for question, response in zip(batch, responses):
+        value = get_value(response)
+        if value is None:
+            print("WARNING: failed to adhere desired output format: ", response)
+        datapoint = {
+            "prompt": question.prompt,
+            "response": response,
+            "value": value,
+            "gt_answer": question.answer,
+            "question_def": question.__dict__,
+        }
+        datapoints.append(datapoint)
 
 with open("gpt3.5-turbo-instruct.json", "w") as f:
     f.write(json.dumps(datapoints, indent=2))
